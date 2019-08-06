af2f61e83aca02990289e252fc44bc8ac49181eb
Dongsik <dongsiku+git@gmail.com>
2019-08-06 14:21:19 -0400

    Fix bug: when the cancel button was clicked, the application is finished

--- powershell/video_to_picture_intervals.ps1
+++ powershell/video_to_picture_intervals.ps1
@@ -1,19 +1,11 @@
-Param(
-  [Parameter()]
-  [String] $FilePath
-)
-
 if([string]::IsNullOrEmpty($FilePath) -Or (Test-Path -LiteralPath $FilePath -PathType Leaf) -eq $false) {
-    [void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")    
-    $dialog = New-Object System.Windows.Forms.OpenFileDialog
-    $dialog.Filter = "VIDEO FILE(*.MOV;*.MP4;*.AVI)|*.MOV;*.MP4;*.AVI"
-    $dialog.InitialDirectory = "~/Desktop"
-    $dialog.Title = "Choose the video file"
+  [void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")    
+  $dialog = New-Object System.Windows.Forms.OpenFileDialog
+  $dialog.Filter = "VIDEO FILE(*.MOV;*.MP4;*.AVI)|*.MOV;*.MP4;*.AVI"
+  $dialog.InitialDirectory = "~/Desktop"
+  $dialog.Title = "Choose the video file"
 
-    if($dialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::NG){
-        exit 1
-    }
-
-    $FilePath = $dialog.FileName
+  if($dialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK){
+    ./python-3.6.8-embed-amd64/python ./src/video_to_picture_intervals.py $dialog.FileName
+  }
 }
-./python-3.6.8-embed-amd64/python ./src/video_to_picture_intervals.py $FilePath
