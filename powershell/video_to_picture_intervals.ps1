70de76fe1b08730a2ba970d35d7f2a193e1b93c9
Dongsik <dongsiku+git@gmail.com>
2019-08-07 07:24:05 -0400

    Remove no need if-statement

--- powershell/video_to_picture_intervals.ps1
+++ powershell/video_to_picture_intervals.ps1
@@ -1,11 +1,9 @@
-if([string]::IsNullOrEmpty($FilePath) -Or (Test-Path -LiteralPath $FilePath -PathType Leaf) -eq $false) {
-  [void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")    
-  $dialog = New-Object System.Windows.Forms.OpenFileDialog
-  $dialog.Filter = "VIDEO FILE(*.MOV;*.MP4;*.AVI)|*.MOV;*.MP4;*.AVI"
-  $dialog.InitialDirectory = "~/Desktop"
-  $dialog.Title = "Choose the video file"
+[void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")    
+$dialog = New-Object System.Windows.Forms.OpenFileDialog
+$dialog.Filter = "VIDEO FILE(*.MOV;*.MP4;*.AVI)|*.MOV;*.MP4;*.AVI"
+$dialog.InitialDirectory = "~/Desktop"
+$dialog.Title = "Choose the video file"
 
-  if($dialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK){
-    ./python-3.6.8-embed-amd64/python ./src/video_to_picture_intervals.py $dialog.FileName
-  }
+if($dialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK){
+  ./python-3.6.8-embed-amd64/python ./src/video_to_picture_intervals.py $dialog.FileName
 }
